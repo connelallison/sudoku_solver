6961427f640af58798ffb7e5170665c6958b62b8
Connel Allison <connelallison@hotmail.com>
2018-11-18 20:34:55 +0000

    cleaned up clutter and discarded code

--- specs/sudoku_solver_spec.rb
+++ specs/sudoku_solver_spec.rb
@@ -5,76 +5,6 @@ require_relative('../sudoku_solver')
 class TestSudokuSolver < Minitest::Test
 
   def setup
-# remember you need instance variables within the setup
-    #
-    # ## TEST UNSOLVED SUDOKU
-    # @row_1 = [0, 0, 2, 9, 8, 0, 5, 0, 0]
-    # @row_2 = [4, 0, 0, 0, 7, 0, 0, 1, 3]
-    # @row_3 = [0, 3, 9, 6, 0, 4, 0, 7, 0]
-    # @row_4 = [2, 0, 0, 0, 5, 6, 4, 0, 0]
-    # @row_5 = [8, 4, 0, 3, 0, 0, 2, 0, 1]
-    # @row_6 = [9, 0, 7, 0, 0, 1, 0, 8, 6]
-    # @row_7 = [6, 0, 0, 7, 0, 5, 1, 3, 0]
-    # @row_8 = [0, 9, 1, 4, 0, 0, 0, 0, 5]
-    # @row_9 = [0, 2, 0, 0, 3, 0, 6, 0, 8]
-    #
-    # @column_1 = [@row_1[0], @row_2[0], @row_3[0], @row_4[0], @row_5[0], @row_6[0], @row_7[0], @row_8[0], @row_9[0]]
-    # @column_2 = [@row_1[1], @row_2[1], @row_3[1], @row_4[1], @row_5[1], @row_6[1], @row_7[1], @row_8[1], @row_9[1]]
-    # @column_3 = [@row_1[2], @row_2[2], @row_3[2], @row_4[2], @row_5[2], @row_6[2], @row_7[2], @row_8[2], @row_9[2]]
-    # @column_4 = [@row_1[3], @row_2[3], @row_3[3], @row_4[3], @row_5[3], @row_6[3], @row_7[3], @row_8[3], @row_9[3]]
-    # @column_5 = [@row_1[4], @row_2[4], @row_3[4], @row_4[4], @row_5[4], @row_6[4], @row_7[4], @row_8[4], @row_9[4]]
-    # @column_6 = [@row_1[5], @row_2[5], @row_3[5], @row_4[5], @row_5[5], @row_6[5], @row_7[5], @row_8[5], @row_9[5]]
-    # @column_7 = [@row_1[6], @row_2[6], @row_3[6], @row_4[6], @row_5[6], @row_6[6], @row_7[6], @row_8[6], @row_9[6]]
-    # @column_8 = [@row_1[7], @row_2[7], @row_3[7], @row_4[7], @row_5[7], @row_6[7], @row_7[7], @row_8[7], @row_9[7]]
-    # @column_9 = [@row_1[8], @row_2[8], @row_3[8], @row_4[8], @row_5[8], @row_6[8], @row_7[8], @row_8[8], @row_9[8]]
-    #
-    # @square_1 = [@row_1[0], @row_1[1], @row_1[2], @row_2[0], @row_2[1], @row_2[2], @row_3[0], @row_3[1], @row_3[2]]
-    # @square_2 = [@row_1[3], @row_1[4], @row_1[5], @row_2[3], @row_2[4], @row_2[5], @row_3[3], @row_3[4], @row_3[5]]
-    # @square_3 = [@row_1[6], @row_1[7], @row_1[8], @row_2[6], @row_2[7], @row_2[8], @row_3[6], @row_3[7], @row_3[8]]
-    # @square_4 = [@row_4[0], @row_4[1], @row_4[2], @row_5[0], @row_5[1], @row_5[2], @row_6[0], @row_6[1], @row_6[2]]
-    # @square_5 = [@row_4[3], @row_4[4], @row_4[5], @row_5[3], @row_5[4], @row_5[5], @row_6[3], @row_6[4], @row_6[5]]
-    # @square_6 = [@row_4[6], @row_4[7], @row_4[8], @row_5[6], @row_5[7], @row_5[8], @row_6[6], @row_6[7], @row_6[8]]
-    # @square_7 = [@row_7[0], @row_7[1], @row_7[2], @row_8[0], @row_8[1], @row_8[2], @row_9[0], @row_9[1], @row_9[2]]
-    # @square_8 = [@row_7[3], @row_7[4], @row_7[5], @row_8[3], @row_8[4], @row_8[5], @row_9[3], @row_9[4], @row_9[5]]
-    # @square_9 = [@row_7[6], @row_7[7], @row_7[8], @row_8[6], @row_8[7], @row_8[8], @row_9[6], @row_9[7], @row_9[8]]
-    #
-    #
-    # ## TEST COMPLETED SUDOKU
-    # @c_row_1 = [1, 7, 2, 9, 8, 3, 5, 6, 4]
-    # @c_row_2 = [4, 6, 8, 5, 7, 2, 9, 1, 3]
-    # @c_row_3 = [5, 3, 9, 6, 1, 4, 8, 7, 2]
-    # @c_row_4 = [2, 1, 3, 8, 5, 6, 4, 9, 7]
-    # @c_row_5 = [8, 4, 6, 3, 9, 7, 2, 5, 1]
-    # @c_row_6 = [9, 5, 7, 2, 4, 1, 3, 8, 6]
-    # @c_row_7 = [6, 8, 4, 7, 2, 5, 1, 3, 9]
-    # @c_row_8 = [3, 9, 1, 4, 6, 8, 7, 2, 5]
-    # @c_row_9 = [7, 2, 5, 1, 3, 9, 6, 4, 8]
-    #
-    # @c_column_1 = [@c_row_1[0], @c_row_2[0], @c_row_3[0], @c_row_4[0], @c_row_5[0], @c_row_6[0], @c_row_7[0], @c_row_8[0], @c_row_9[0]]
-    # @c_column_2 = [@c_row_1[1], @c_row_2[1], @c_row_3[1], @c_row_4[1], @c_row_5[1], @c_row_6[1], @c_row_7[1], @c_row_8[1], @c_row_9[1]]
-    # @c_column_3 = [@c_row_1[2], @c_row_2[2], @c_row_3[2], @c_row_4[2], @c_row_5[2], @c_row_6[2], @c_row_7[2], @c_row_8[2], @c_row_9[2]]
-    # @c_column_4 = [@c_row_1[3], @c_row_2[3], @c_row_3[3], @c_row_4[3], @c_row_5[3], @c_row_6[3], @c_row_7[3], @c_row_8[3], @c_row_9[3]]
-    # @c_column_5 = [@c_row_1[4], @c_row_2[4], @c_row_3[4], @c_row_4[4], @c_row_5[4], @c_row_6[4], @c_row_7[4], @c_row_8[4], @c_row_9[4]]
-    # @c_column_6 = [@c_row_1[5], @c_row_2[5], @c_row_3[5], @c_row_4[5], @c_row_5[5], @c_row_6[5], @c_row_7[5], @c_row_8[5], @c_row_9[5]]
-    # @c_column_7 = [@c_row_1[6], @c_row_2[6], @c_row_3[6], @c_row_4[6], @c_row_5[6], @c_row_6[6], @c_row_7[6], @c_row_8[6], @c_row_9[6]]
-    # @c_column_8 = [@c_row_1[7], @c_row_2[7], @c_row_3[7], @c_row_4[7], @c_row_5[7], @c_row_6[7], @c_row_7[7], @c_row_8[7], @c_row_9[7]]
-    # @c_column_9 = [@c_row_1[8], @c_row_2[8], @c_row_3[8], @c_row_4[8], @c_row_5[8], @c_row_6[8], @c_row_7[8], @c_row_8[8], @c_row_9[8]]
-    #
-    # @c_square_1 = [@c_row_1[0], @c_row_1[1], @c_row_1[2], @c_row_2[0], @c_row_2[1], @c_row_2[2], @c_row_3[0], @c_row_3[1], @c_row_3[2]]
-    # @c_square_2 = [@c_row_1[3], @c_row_1[4], @c_row_1[5], @c_row_2[3], @c_row_2[4], @c_row_2[5], @c_row_3[3], @c_row_3[4], @c_row_3[5]]
-    # @c_square_3 = [@c_row_1[6], @c_row_1[7], @c_row_1[8], @c_row_2[6], @c_row_2[7], @c_row_2[8], @c_row_3[6], @c_row_3[7], @c_row_3[8]]
-    # @c_square_4 = [@c_row_4[0], @c_row_4[1], @c_row_4[2], @c_row_5[0], @c_row_5[1], @c_row_5[2], @c_row_6[0], @c_row_6[1], @c_row_6[2]]
-    # @c_square_5 = [@c_row_4[3], @c_row_4[4], @c_row_4[5], @c_row_5[3], @c_row_5[4], @c_row_5[5], @c_row_6[3], @c_row_6[4], @c_row_6[5]]
-    # @c_square_6 = [@c_row_4[6], @c_row_4[7], @c_row_4[8], @c_row_5[6], @c_row_5[7], @c_row_5[8], @c_row_6[6], @c_row_6[7], @c_row_6[8]]
-    # @c_square_7 = [@c_row_7[0], @c_row_7[1], @c_row_7[2], @c_row_8[0], @c_row_8[1], @c_row_8[2], @c_row_9[0], @c_row_9[1], @c_row_9[2]]
-    # @c_square_8 = [@c_row_7[3], @c_row_7[4], @c_row_7[5], @c_row_8[3], @c_row_8[4], @c_row_8[5], @c_row_9[3], @c_row_9[4], @c_row_9[5]]
-    # @c_square_9 = [@c_row_7[6], @c_row_7[7], @c_row_7[8], @c_row_8[6], @c_row_8[7], @c_row_8[8], @c_row_9[6], @c_row_9[7], @c_row_9[8]]
-    #
-    # @sudoku_1 = [@row_1, @row_2, @row_3, @row_4, @row_5, @row_6, @row_7, @row_8, @row_9]
-    # @c_sudoku_1 = [@c_row_1, @c_row_2, @c_row_3, @c_row_4, @c_row_5, @c_row_6, @c_row_7, @c_row_8, @c_row_9]
-    #
-    # @test_unit = [4, 7, 6, 3, 8, 9, 2, 1, 5]
-
 
     @sudoku_1_rows = [
       [0, 0, 2, 9, 8, 0, 5, 0, 0],
@@ -100,7 +30,6 @@ class TestSudokuSolver < Minitest::Test
     [3, 9, 1, 4, 6, 8, 7, 2, 5],
     [7, 2, 5, 1, 3, 9, 6, 4, 8]
     ]
-
     @c_sudoku_1 = Sudoku.new(@c_sudoku_1_rows)
   end
 
--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,78 +1,7 @@
-# @row_1 = [0, 0, 2, 9, 8, 0, 5, 0, 0]
-# @row_2 = [4, 0, 0, 0, 7, 0, 0, 1, 3]
-# @row_3 = [0, 3, 9, 6, 0, 4, 0, 7, 0]
-# @row_4 = [2, 0, 0, 0, 5, 6, 4, 0, 0]
-# @row_5 = [8, 4, 0, 3, 0, 0, 2, 0, 1]
-# @row_6 = [9, 0, 7, 0, 0, 1, 0, 8, 6]
-# @row_7 = [6, 0, 0, 7, 0, 5, 1, 3, 0]
-# @row_8 = [0, 9, 1, 4, 0, 0, 0, 0, 5]
-# @row_9 = [0, 2, 0, 0, 3, 0, 6, 0, 8]
-#
-# @column_1 = [@row_1[0], @row_2[0], @row_3[0], @row_4[0], @row_5[0], @row_6[0], @row_7[0], @row_8[0], @row_9[0]]
-# @column_2 = [@row_1[1], @row_2[1], @row_3[1], @row_4[1], @row_5[1], @row_6[1], @row_7[1], @row_8[1], @row_9[1]]
-# @column_3 = [@row_1[2], @row_2[2], @row_3[2], @row_4[2], @row_5[2], @row_6[2], @row_7[2], @row_8[2], @row_9[2]]
-# @column_4 = [@row_1[3], @row_2[3], @row_3[3], @row_4[3], @row_5[3], @row_6[3], @row_7[3], @row_8[3], @row_9[3]]
-# @column_5 = [@row_1[4], @row_2[4], @row_3[4], @row_4[4], @row_5[4], @row_6[4], @row_7[4], @row_8[4], @row_9[4]]
-# @column_6 = [@row_1[5], @row_2[5], @row_3[5], @row_4[5], @row_5[5], @row_6[5], @row_7[5], @row_8[5], @row_9[5]]
-# @column_7 = [@row_1[6], @row_2[6], @row_3[6], @row_4[6], @row_5[6], @row_6[6], @row_7[6], @row_8[6], @row_9[6]]
-# @column_8 = [@row_1[7], @row_2[7], @row_3[7], @row_4[7], @row_5[7], @row_6[7], @row_7[7], @row_8[7], @row_9[7]]
-# @column_9 = [@row_1[8], @row_2[8], @row_3[8], @row_4[8], @row_5[8], @row_6[8], @row_7[8], @row_8[8], @row_9[8]]
-#
-# @square_1 = [@row_1[0], @row_1[1], @row_1[2], @row_2[0], @row_2[1], @row_2[2], @row_3[0], @row_3[1], @row_3[2]]
-# @square_2 = [@row_1[3], @row_1[4], @row_1[5], @row_2[3], @row_2[4], @row_2[5], @row_3[3], @row_3[4], @row_3[5]]
-# @square_3 = [@row_1[6], @row_1[7], @row_1[8], @row_2[6], @row_2[7], @row_2[8], @row_3[6], @row_3[7], @row_3[8]]
-# @square_4 = [@row_4[0], @row_4[1], @row_4[2], @row_5[0], @row_5[1], @row_5[2], @row_6[0], @row_6[1], @row_6[2]]
-# @square_5 = [@row_4[3], @row_4[4], @row_4[5], @row_5[3], @row_5[4], @row_5[5], @row_6[3], @row_6[4], @row_6[5]]
-# @square_6 = [@row_4[6], @row_4[7], @row_4[8], @row_5[6], @row_5[7], @row_5[8], @row_6[6], @row_6[7], @row_6[8]]
-# @square_7 = [@row_7[0], @row_7[1], @row_7[2], @row_8[0], @row_8[1], @row_8[2], @row_9[0], @row_9[1], @row_9[2]]
-# @square_8 = [@row_7[3], @row_7[4], @row_7[5], @row_8[3], @row_8[4], @row_8[5], @row_9[3], @row_9[4], @row_9[5]]
-# @square_9 = [@row_7[6], @row_7[7], @row_7[8], @row_8[6], @row_8[7], @row_8[8], @row_9[6], @row_9[7], @row_9[8]]
-#
-# @sudoku_1 = [@row_1, @row_2, @row_3, @row_4, @row_5, @row_6, @row_7, @row_8, @row_9]
-
-# p @row_1.sort() == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 class Sudoku
   attr_accessor :sudoku, :rows, :columns, :squares, :row_associations, :column_associations, :square_associations, :complete_unit#, :row_1, :row_2, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9, :column_1, :column_2, :column_4, :column_5, :column_6, :column_7, :column_8, :column_9, :square_1, :square_2, :square_4, :square_5, :square_6, :square_7, :square_8, :square_9
   def initialize(sudoku_rows)
-
-    # @row_1 = sudoku_rows[0]
-    # @row_2 = sudoku_rows[1]
-    # @row_3 = sudoku_rows[2]
-    # @row_4 = sudoku_rows[3]
-    # @row_5 = sudoku_rows[4]
-    # @row_6 = sudoku_rows[5]
-    # @row_7 = sudoku_rows[6]
-    # @row_8 = sudoku_rows[7]
-    # @row_9 = sudoku_rows[8]
-    #
-    # @rows = [@row_1, @row_2, @row_3, @row_4, @row_5, @row_6, @row_7, @row_8, @row_9]
-    #
-    # @column_1 = [@row_1[0], @row_2[0], @row_3[0], @row_4[0], @row_5[0], @row_6[0], @row_7[0], @row_8[0], @row_9[0]]
-    # @column_2 = [@row_1[1], @row_2[1], @row_3[1], @row_4[1], @row_5[1], @row_6[1], @row_7[1], @row_8[1], @row_9[1]]
-    # @column_3 = [@row_1[2], @row_2[2], @row_3[2], @row_4[2], @row_5[2], @row_6[2], @row_7[2], @row_8[2], @row_9[2]]
-    # @column_4 = [@row_1[3], @row_2[3], @row_3[3], @row_4[3], @row_5[3], @row_6[3], @row_7[3], @row_8[3], @row_9[3]]
-    # @column_5 = [@row_1[4], @row_2[4], @row_3[4], @row_4[4], @row_5[4], @row_6[4], @row_7[4], @row_8[4], @row_9[4]]
-    # @column_6 = [@row_1[5], @row_2[5], @row_3[5], @row_4[5], @row_5[5], @row_6[5], @row_7[5], @row_8[5], @row_9[5]]
-    # @column_7 = [@row_1[6], @row_2[6], @row_3[6], @row_4[6], @row_5[6], @row_6[6], @row_7[6], @row_8[6], @row_9[6]]
-    # @column_8 = [@row_1[7], @row_2[7], @row_3[7], @row_4[7], @row_5[7], @row_6[7], @row_7[7], @row_8[7], @row_9[7]]
-    # @column_9 = [@row_1[8], @row_2[8], @row_3[8], @row_4[8], @row_5[8], @row_6[8], @row_7[8], @row_8[8], @row_9[8]]
-    #
-    # @columns = [@column_1, @column_2, @column_3, @column_4, @column_5, @column_6, @column_7, @column_8, @column_9]
-    #
-    #
-    #
-    # @square_1 = [@row_1[0], @row_1[1], @row_1[2], @row_2[0], @row_2[1], @row_2[2], @row_3[0], @row_3[1], @row_3[2]]
-    # @square_2 = [@row_1[3], @row_1[4], @row_1[5], @row_2[3], @row_2[4], @row_2[5], @row_3[3], @row_3[4], @row_3[5]]
-    # @square_3 = [@row_1[6], @row_1[7], @row_1[8], @row_2[6], @row_2[7], @row_2[8], @row_3[6], @row_3[7], @row_3[8]]
-    # @square_4 = [@row_4[0], @row_4[1], @row_4[2], @row_5[0], @row_5[1], @row_5[2], @row_6[0], @row_6[1], @row_6[2]]
-    # @square_5 = [@row_4[3], @row_4[4], @row_4[5], @row_5[3], @row_5[4], @row_5[5], @row_6[3], @row_6[4], @row_6[5]]
-    # @square_6 = [@row_4[6], @row_4[7], @row_4[8], @row_5[6], @row_5[7], @row_5[8], @row_6[6], @row_6[7], @row_6[8]]
-    # @square_7 = [@row_7[0], @row_7[1], @row_7[2], @row_8[0], @row_8[1], @row_8[2], @row_9[0], @row_9[1], @row_9[2]]
-    # @square_8 = [@row_7[3], @row_7[4], @row_7[5], @row_8[3], @row_8[4], @row_8[5], @row_9[3], @row_9[4], @row_9[5]]
-    # @square_9 = [@row_7[6], @row_7[7], @row_7[8], @row_8[6], @row_8[7], @row_8[8], @row_9[6], @row_9[7], @row_9[8]]
-    #
-    # @squares = [@square_1, @square_2, @square_3, @square_4, @square_5, @square_6, @square_7, @square_8, @square_9]
 
     @rows = [sudoku_rows[0], sudoku_rows[1], sudoku_rows[2], sudoku_rows[3], sudoku_rows[4], sudoku_rows[5], sudoku_rows[6], sudoku_rows[7], sudoku_rows[8]]
 
@@ -135,9 +64,7 @@ class Sudoku
     ]
 
     @sudoku = [@rows, @columns, @squares]
-
     @complete_unit = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
   end
 
   def unit_complete?(unit)
@@ -148,7 +75,7 @@ class Sudoku
     return sudoku.sudoku.all?() { |units| units.all?() { |unit| sudoku.unit_complete?(unit) == true } }
   end
 
-  ## Really not convinced this is the best way of going about things.
+  ## Really not convinced this is the best way of going about things, but I guess it works.
   def reassign()
     @columns = [[@rows[0][0], @rows[1][0], @rows[2][0], @rows[3][0], @rows[4][0], @rows[5][0], @rows[6][0], @rows[7][0], @rows[8][0]],
     [@rows[0][1], @rows[1][1], @rows[2][1], @rows[3][1], @rows[4][1], @rows[5][1], @rows[6][1], @rows[7][1], @rows[8][1]],
@@ -193,7 +120,6 @@ class Sudoku
   end
 
   def missing_numbers(unit)
-    # reject may cause trouble with returning nil - keep an eye on this
     @complete_unit.reject() { |num| unit.include?(num) }
   end
 
@@ -201,8 +127,6 @@ class Sudoku
     empty_spaces = @square_associations[@squares.index(square)][2].select() {
       |coordinates| (coordinate(coordinates[0], coordinates[1]) == 0)
     }
-    # p empty_spaces
-    # p @square_associations[@squares.index(square)][2]
     if (empty_spaces != nil)
       return empty_spaces
     else
@@ -216,13 +140,8 @@ class Sudoku
   end
 
   def crosshatch(square, num)
-    # p square
-    # p "test"
-    # p @square_associations[@squares.index(square)][2]
     empty_spaces = empty_spaces(square)
     # p "Before elimination: #{empty_spaces}"
-    # missing_numbers = @complete_unit.reject() { |number| square.include?(number) }
-    # p missing_numbers
     unless (empty_spaces == "skip")
       if (empty_spaces.count() > 1)
         @square_associations[@squares.index(square)][0].each() { |row| #p "Row #{@rows.index(row)}"
@@ -241,36 +160,11 @@ class Sudoku
         }
       end
       if (empty_spaces.count() == 1)
-        # @rows[empty_spaces[0][0]][empty_spaces[0][1]] =  num
-        # reassign()
-        # p square
-        # p square
-        # p square
-        # p square
-        # missing_numbers = @complete_unit.reject() { |number| square.include?(number) }
-        # p missing_numbers
         return [empty_spaces[0][0], empty_spaces[0][1], num]
       end
     end
       return "skip"
   end
-
-  # def solve_square(square)
-  #   missing_numbers = missing_numbers(square)
-  #   p missing_numbers
-  #   p square
-  #   missing_numbers.each() { |number|
-  #     p @squares.index(quare)
-  #     p "Checking #{number}"
-  #     p change = crosshatch(quare, number)
-  #     unless (change == "skip" )
-  #       square = fill_in(change[0], change[1], change[2])
-  #     end
-  #     p missing_numbers
-  #     p square
-  #   }
-  #
-  # end
 
   def solve_sudoku(sudoku)
     sudoku.rows.each() { |row| p row }
@@ -317,10 +211,7 @@ sudoku_1_rows = [
   [0, 9, 1, 4, 0, 0, 0, 0, 5],
   [0, 2, 0, 0, 3, 0, 6, 0, 8]
 ]
-
 sudoku_1 = Sudoku.new(sudoku_1_rows)
-
-# p sudoku_1.square_1
 
 c_sudoku_1_rows = [
 [1, 7, 2, 9, 8, 3, 5, 6, 4],
@@ -333,105 +224,10 @@ c_sudoku_1_rows = [
 [3, 9, 1, 4, 6, 8, 7, 2, 5],
 [7, 2, 5, 1, 3, 9, 6, 4, 8]
 ]
-
 c_sudoku_1 = Sudoku.new(c_sudoku_1_rows)
 
-# p c_sudoku_1.square_1
-# @row_1 = [0, 0, 2, 9, 8, 0, 5, 0, 0]
-# @row_2 = [4, 0, 0, 0, 7, 0, 0, 1, 3]
-# @row_3 = [0, 3, 9, 6, 0, 4, 0, 7, 0]
-# @row_4 = [2, 0, 0, 0, 5, 6, 4, 0, 0]
-# @row_5 = [8, 4, 0, 3, 0, 0, 2, 0, 1]
-# @row_6 = [9, 0, 7, 0, 0, 1, 0, 8, 6]
-# @row_7 = [6, 0, 0, 7, 0, 5, 1, 3, 0]
-# @row_8 = [0, 9, 1, 4, 0, 0, 0, 0, 5]
-# @row_9 = [0, 2, 0, 0, 3, 0, 6, 0, 8]
-
-
-
-
-# p sudoku_1.unit_complete?(sudoku_1.rows[0])
-# p sudoku_1.sudoku.all?() { |units| units.all?() { |unit| sudoku_1.unit_complete?(unit) == true } }
-# p c_sudoku_1.sudoku.all?() { |units| units.all?() { |unit| c_sudoku_1.unit_complete?(unit) == true } }
-# p sudoku_1.sudoku_complete?(sudoku_1)
-# p c_sudoku_1.sudoku_complete?(c_sudoku_1)
-#
-# p sudoku_1.squares[0]
-# p sudoku_1.complete_unit.reject() { |num| sudoku_1.squares[0].include?(num) }
-
-# p sudoku_1.squares[0][0].object_id() == sudoku_1.rows[0][0].object_id()
-#
-# sudoku_1.rows[0][0] = 3
-# sudoku_1.reassign()
-# p sudoku_1.rows[0][0]
-# p sudoku_1.squares[0][0]
-# p sudoku_1.squares[0][0].object_id() == sudoku_1.rows[0][0].object_id()
-
-# p sudoku_1.rows[2][4]
-# p sudoku_1.columns[4][2]
-# p sudoku_1.squares[1][7]
-# sudoku_1.rows[2][sudoku_1.columns.index(sudoku_1.columns[4])] = 1
-# sudoku_1.reassign()
-# p sudoku_1.rows[2][4]
-# p sudoku_1.columns[4][2]
-# p sudoku_1.squares[1][7]
-#
-# p sudoku_1
-
-# p sudoku_1.missing_numbers(sudoku_1.squares[3])
-# p sudoku_1.fill_in(sudoku_1.rows[2], sudoku_1.columns[4], 1)
-
-# p sudoku_1.square_associations[sudoku_1.squares[1]][0]
-# p sudoku_1.square_associations[sudoku_1.squares[1]][0].select() { |row| row.include?(1)}
-
-# sudoku_1.rows[2][4]
-
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# p c_sudoku_1.empty_spaces(c_sudoku_1.squares[1])
-
-# p [1, 2][0]
-
-# p sudoku_1.coordinate(2, 4)
-
-# p sudoku_1.squares[1]
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# p sudoku_1.square_associations[1][2]
-# p sudoku_1.rows[2]
-# p sudoku_1.columns[4]
-
-# p sudoku_1.missing_numbers(sudoku_1.squares[1])
-# p sudoku_1.squares[1]
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# change = sudoku_1.crosshatch(sudoku_1.squares[1], 1)
-# # p sudoku_1.square_associations[1][2]
-# sudoku_1.fill_in(change[0], change[1], change[2])
-# # p sudoku_1.square_associations[1][2]
-# p sudoku_1.missing_numbers(sudoku_1.squares[1])
-# # p sudoku_1.rows[2]
-# # p sudoku_1.columns[4]
-# p sudoku_1.squares[1]
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# # p sudoku_1.missing_numbers(sudoku_1.rows[2])
-# # p sudoku_1.missing_numbers(sudoku_1.columns[4])
-# # p sudoku_1.missing_numbers(sudoku_1.squares[1])
-# change = sudoku_1.crosshatch(sudoku_1.squares[1], 2)
-# # p sudoku_1.rows[2]
-# # p sudoku_1.columns[4]
-# p sudoku_1.missing_numbers(sudoku_1.squares[1])
-# p sudoku_1.squares[1]
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# change = sudoku_1.crosshatch(sudoku_1.squares[1], 3)
-# sudoku_1.fill_in(change[0], change[1], change[2])
-# p sudoku_1.missing_numbers(sudoku_1.squares[1])
-# # p sudoku_1.rows[2]
-# # p sudoku_1.columns[4]
-# p sudoku_1.squares[1]
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# change = sudoku_1.crosshatch(sudoku_1.squares[1], 5)
-# sudoku_1.fill_in(change[0], change[1], change[2])
-# p sudoku_1.missing_numbers(sudoku_1.squares[1])
-# p sudoku_1.squares[1]
-# p sudoku_1.empty_spaces(sudoku_1.squares[1])
+## WORKING METHOD TO SOLVE SUDOKU PUZZLE
+# sudoku_1.solve_sudoku(sudoku_1)
 
 
 ## WORKING CODE TO CROSSHATCH A GIVEN SQUARE
@@ -453,65 +249,7 @@ c_sudoku_1 = Sudoku.new(c_sudoku_1_rows)
 # sudoku_1.rows.each() { |row| p row }
 
 
-# sudoku_1.rows.each() { |row| p row }
-# (0..8).each() { |squarenum|
-# p sudoku_1.missing_numbers(sudoku_1.squares[squarenum])
-# p sudoku_1.squares[squarenum]
-# # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# sudoku_1.missing_numbers(sudoku_1.squares[squarenum]).each() { |num|
-#   p "Checking #{num}"
-#   p change = sudoku_1.crosshatch(sudoku_1.squares[squarenum], num)
-#   unless (change == "skip")
-#     sudoku_1.fill_in(change[0], change[1], change[2])
-#   end
-#   p sudoku_1.missing_numbers(sudoku_1.squares[squarenum])
-#   p sudoku_1.squares[1]
-#   # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# }
-# }
-# p "-------------------------"
-# sudoku_1.rows.each() { |row| p row }
-#
-# (0..8).each() { |squarenum|
-# p sudoku_1.missing_numbers(sudoku_1.squares[squarenum])
-# p sudoku_1.squares[squarenum]
-# # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# sudoku_1.missing_numbers(sudoku_1.squares[squarenum]).each() { |num|
-#   p "Checking #{num}"
-#   p change = sudoku_1.crosshatch(sudoku_1.squares[squarenum], num)
-#   unless (change == "skip")
-#     sudoku_1.fill_in(change[0], change[1], change[2])
-#   end
-#   p sudoku_1.missing_numbers(sudoku_1.squares[squarenum])
-#   p sudoku_1.squares[1]
-#   # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# }
-# }
-# p "-------------------------"
-# sudoku_1.rows.each() { |row| p row }
-#
-# (0..8).each() { |squarenum|
-# p sudoku_1.missing_numbers(sudoku_1.squares[squarenum])
-# p sudoku_1.squares[squarenum]
-# # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# sudoku_1.missing_numbers(sudoku_1.squares[squarenum]).each() { |num|
-#   p "Checking #{num}"
-#   p change = sudoku_1.crosshatch(sudoku_1.squares[squarenum], num)
-#   unless (change == "skip")
-#     sudoku_1.fill_in(change[0], change[1], change[2])
-#   end
-#   p sudoku_1.missing_numbers(sudoku_1.squares[squarenum])
-#   p sudoku_1.squares[1]
-#   # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# }
-# }
-# p "-------------------------"
-# sudoku_1.rows.each() { |row| p row }
-
-
-
-
-
+## WORKING CODE TO SOLVE SUDOKU PUZZLE
 # sudoku_1.rows.each() { |row| p row }
 # count = 0
 # p "Number of passes: #{count}"
@@ -540,104 +278,3 @@ c_sudoku_1 = Sudoku.new(c_sudoku_1_rows)
 # if (sudoku_1.sudoku_complete?(sudoku_1) == true)
 #   p "Sudoku puzzle complete!"
 # end
-
-sudoku_1.solve_sudoku(sudoku_1)
-
-
-
-
-
-
-# [0, 0, 2, 9, 8, 0, 5, 0, 0]
-# [4, 0, 0, 0, 7, 0, 0, 1, 3]
-# [0, 3, 9, 6, 0, 4, 0, 7, 0]
-# [2, 0, 0, 0, 5, 6, 4, 0, 0]
-# [8, 4, 0, 3, 0, 0, 2, 0, 1]
-# [9, 0, 7, 0, 0, 1, 0, 8, 6]
-# [6, 0, 0, 7, 0, 5, 1, 3, 0]
-# [0, 9, 1, 4, 0, 0, 0, 0, 5]
-# [0, 2, 0, 0, 3, 0, 6, 0, 8]
-#
-# [0, 0, 2, 9, 8, 3, 5, 6, 0]
-# [4, 0, 0, 5, 7, 0, 9, 1, 3]
-# [0, 3, 9, 6, 1, 4, 0, 7, 2]
-# [2, 1, 3, 8, 5, 6, 4, 9, 7]
-# [8, 4, 6, 3, 9, 7, 2, 5, 1]
-# [9, 0, 7, 0, 4, 1, 3, 8, 6]
-# [6, 0, 0, 7, 0, 5, 1, 3, 9]
-# [3, 9, 1, 4, 6, 8, 7, 2, 5]
-# [7, 2, 0, 1, 3, 9, 6, 0, 8]
-#
-# [1, 7, 2, 9, 8, 3, 5, 6, 4]
-# [4, 6, 8, 5, 7, 2, 9, 1, 3]
-# [5, 3, 9, 6, 1, 4, 8, 7, 2]
-# [2, 1, 3, 8, 5, 6, 4, 9, 7]
-# [8, 4, 6, 3, 9, 7, 2, 5, 1]
-# [9, 5, 7, 2, 4, 1, 3, 8, 6]
-# [6, 8, 0, 7, 2, 5, 1, 3, 9]
-# [3, 9, 1, 4, 6, 8, 7, 2, 5]
-# [7, 2, 5, 1, 3, 9, 6, 4, 8
-#
-# [1, 7, 2, 9, 8, 3, 5, 6, 4]
-# [4, 6, 8, 5, 7, 2, 9, 1, 3]
-# [5, 3, 9, 6, 1, 4, 8, 7, 2]
-# [2, 1, 3, 8, 5, 6, 4, 9, 7]
-# [8, 4, 6, 3, 9, 7, 2, 5, 1]
-# [9, 5, 7, 2, 4, 1, 3, 8, 6]
-# [6, 8, 4, 7, 2, 5, 1, 3, 9]
-# [3, 9, 1, 4, 6, 8, 7, 2, 5]
-# [7, 2, 5, 1, 3, 9, 6, 4, 8]
-
-# complete_square = Proc.new { |square, num|
-#   p "Checking #{num}"
-#   p change = sudoku_1.crosshatch(square, num)
-#   unless (change == "skip")
-#     sudoku_1.fill_in(change[0], change[1], change[2])
-#   end
-#   p sudoku_1.missing_numbers(square)
-#   p square
-
-
-  # p sudoku_1.empty_spaces(sudoku_1.squares[1])
-# }
-# sudoku_1.rows.each() { |row| p row }
-# sudoku_1.squares.each() { |square|
-#   missing_numbers = sudoku_1.missing_numbers(square)
-#   p "-------------------------"
-#   p missing_numbers
-#   missing_numbers.each() { |num|
-#     change = sudoku_1.crosshatch(square, num)
-#     unless (change == "skip")
-#       sudoku_1.fill_in(change[0], change[1], change[2])
-#     end
-#   }
-#   p "-------------------------"
-#   sudoku_1.rows.each() { |row| p row }
-# }
-
-# p sudoku_1.rows[2][4]
-# p sudoku_1.columns[5][0]
-# p sudoku_1.squares[2][3]
-# p sudoku_1.rows[2][4].object_id()
-# p sudoku_1.columns[5][0].object_id()
-# p sudoku_1.squares[2][3].object_id()
-
-
-# p sudoku_1.column_associations[4]
-# p [[1, 3], [1, 5]].delete_if() { |coordinates| sudoku_1.column_associations[4].include?(coordinates) }
-# p sudoku_1.column_associations[5]
-# p [[1, 3], [1, 5]].delete_if() { |coordinates| sudoku_1.column_associations[5].include?(coordinates) }
-
-# p sudoku_1.squares[1]
-# sudoku_1.solve_square(sudoku_1.squares[1])
-# p sudoku_1.squares[1]
-  # empty_spaces = sudoku_1.empty_spaces(sudoku_1.squares[1])
-  # p empty_spaces
-  # sudoku_1.square_associations[sudoku_1.squares[1]][0].each() { |row| if (row.include?(1))
-  #   empty_spaces.reject!() { |coordinates| @row_associations[sudoku_1.squares[1]].include?(coordinates) }
-  # end
-  # }
-  # p empty_spaces
-
-
-# sudoku_1.solve_square(sudoku_1.squares[1])
